494310029030df8ff8562db359aca37efca88f4f
Jiheon Kim <rlawys4565@gmail.com>
2020-01-02 18:50:59 +0900

    MapEditor - 구조 잡기

---
+++ Assets/Script/Tile/MazeTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public enum ETileType
+{
+	Ground,
+	GroundEmpty,
+	GroundEnemy,
+	Wall,
+	Door
+}
+
+public class MazeTile : MonoBehaviour
+{
+	protected Point point;
+	protected ETileType type;
+}
