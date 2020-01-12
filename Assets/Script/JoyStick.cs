e286883ee4e4dcccab6a65f6d0ba4aafd4051d76
BlueCokeWine <rlawys4565@gmail.com>
2020-01-13 04:00:14 +0900

    조이스틱 버그 & 카메라 렌더 버그 수정

--- Assets/Script/JoyStick.cs
+++ Assets/Script/JoyStick.cs
@@ -3,29 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class JoyStick : MonoBehaviour
 {
 	// 공개
-	[SerializeField] Transform stick;         // 조이스틱.
+	[SerializeField] RectTransform scaleChecker;
+	[SerializeField] RectTransform stick;         // 조이스틱.
 	[SerializeField] Transform up;
 	[SerializeField] Transform down;
 	[SerializeField] Transform right;
 	[SerializeField] Transform left;
+	[SerializeField] CanvasScaler scaler;
 
 	// 비공개
 	Vector3 joyStickOriginPosition;  // 조이스틱의 처음 위치.
 	Vector3 joyStickDirection;         // 조이스틱의 벡터(방향)
-	float radius;           // 조이스틱 배경의 반 지름.
 
-	void Awake()
+	void Start()
 	{
-		radius = GetComponent<RectTransform>().sizeDelta.y * 0.5f;
 		joyStickOriginPosition = stick.transform.position;
-
-		// 캔버스 크기에대한 반지름 조절.
-		float Can = transform.parent.GetComponent<RectTransform>().localScale.x;
-		radius *= Can;
 	}
 
 	// 드래그
@@ -34,7 +31,6 @@ public class JoyStick : MonoBehaviour
 		PointerEventData pointerData = baseData as PointerEventData;
 		Vector3 pointerPosition = pointerData.position;
 
-		// 조이스틱을 이동시킬 방향을 구함.(오른쪽,왼쪽,위,아래)
 		joyStickDirection = (pointerPosition - joyStickOriginPosition).normalized;
 
 		if(Mathf.Abs(joyStickDirection.x) > Mathf.Abs(joyStickDirection.y))
